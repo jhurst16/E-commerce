498245d8a135e7854b8512df753d9b080d6c8952
jhurst16 <jeremyhurst16@gmail.com>
2021-07-19 00:54:27 -0400

    added proper routes, proper routs functions, and connections

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# E-commerce Back End Starter Code
+# E-commerce
+
+Keep track of team using inquirer and mySQL.
+
+## Contributors
+```
+Jeremy Hurst
+```
+
+## Languages
+
+
+```
+JavaScript
+```
+
+## dependencies
+
+```
+mySQL2
+inquirer
+express
+dotenv
+```
+
+## Run Environment
+
+```
+Node.js
+```
+
+## DataBase
+
+```
+mySQL2
+```
+
+## Usage
+
+Coming Soon!
+
+## Links
+
+Github Repository: https://github.com/jhurst16/E-commerce
+Walkthrough Video:  coming soon!
---
+++ routes/api/category-routes.js
@@ -0,0 +1,86 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+    Category.findAll({
+        include: [
+            { model: Product }
+        ]
+    })
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: "Category Not Found" })
+            }
+            res.json(dbCategoryData)
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    Category.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [{ model: Product }]
+    })
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: "Category Not Found" });
+                return;
+            }
+            res.json(dbCategoryData);
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.post('/', (req, res) => {
+    Category.create({
+        category_name: req.body.category_name
+    })
+        .then(dbCategoryData => res.json(dbCategoryData))
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+router.put('/:id', (req, res) => {
+    Category.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: "Category Not Found" });
+                return;
+            }
+            res.json(dbCategoryData)
+        })
+        .catch(err => { res.status(500).json(err) })
+});
+
+router.delete('/:id', (req, res) => {
+    Category.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: "Category Not Found" })
+                return;
+            }
+            res.json(dbCategoryData)
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+});
+
+module.exports = router;
